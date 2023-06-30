2c58698b10ee80a75c9c81e6d7745452176710dd
ariane-ai <134939078+ariane-ai@users.noreply.github.com>
2023-06-30 13:32:12 +0000

    created Authenticator class added authenticated field to Customer

---
+++ src/main/java/bank/Authenticator.java
@@ -0,0 +1,23 @@
+package bank;
+
+import javax.security.auth.login.LoginException;
+
+public class Authenticator {
+  
+  public static Customer login(String username, String password) throws LoginException {
+
+    Customer customer = DataSource.getCustomer(username);
+    if(customer == null){
+      throw new LoginException("username not found");
+    }
+    if(password.equals(customer.getPassword())){
+      customer.setAuthenticated(true);
+      return customer;
+    }
+    else throw new LoginException("password incorrect")  
+  }
+
+  public static void logout(Customer customer){
+    customer.setAuthenticated(false);
+  }
+}
