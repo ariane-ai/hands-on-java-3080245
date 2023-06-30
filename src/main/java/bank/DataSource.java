4fecc9270a8a6071eda975683e5bd296fe0ac75b
ariane-ai <134939078+ariane-ai@users.noreply.github.com>
2023-06-30 12:01:11 +0000

    added getAccount to DataSource

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -13,7 +13,7 @@ public class DataSource {
     Connection connection = null;
     try{
       connection = DriverManager.getConnection(db_file);
-      System.out.println("connected");
+      //System.out.println("connected");
     } catch(SQLException e){
       e.printStackTrace();
     }
@@ -43,8 +43,29 @@ public class DataSource {
     return customer;
   }
 
+  public static Account getAccount(int accountId){
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+
+    try(Connection connection = connect(); // automatically disconnects
+        PreparedStatement statement = connection.prepareStatement(sql)){
+          statement.setInt(1, accountId);
+          try(ResultSet resultSet = statement.executeQuery()){
+            account = new Account(
+              resultSet.getInt("id"),
+              resultSet.getString("type"),
+              resultSet.getDouble("balance")); 
+          }
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+
+    return account;
+  }
+
   public static void main(String[] args){
-    Customer customer = getCustomer("hnegal76@umn.edu");
-    System.out.println(customer.getName());
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    Account account = getAccount(customer.getAccountId());
+    System.out.println(account.getBalance());
   }
 }
